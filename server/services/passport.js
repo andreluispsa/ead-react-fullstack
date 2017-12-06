447d6308624fd9e87326401a832760328fbd2ca0
Andre Sa <andrepena.sa@gmail.com>
2017-12-05 22:56:22 -0200

    Mongoose

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -13,6 +16,18 @@ passport.use(
       console.log('access token: ', accessToken);
       console.log('refresh token: ', refreshToken);
       console.log('profile: ', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have a record with the given profileId
+          console.log('User already exists');
+          done(null, existingUser);
+        } else {
+          // don't have a record with the given profileId, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
